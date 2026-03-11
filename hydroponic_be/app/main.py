dcd0f10c631a2eb2d4eb3a3475e74cdd94aaa87a
ddavviidbunbun <david.bunyamin.99@gmail.com>
2026-03-11 13:20:09 +0700

    add local dev config

--- hydroponic_be/app/main.py
+++ hydroponic_be/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-# from fastapi.routing import APIRoute
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
