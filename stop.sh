36d5cf1ab09436c60569586184c531f1d36dd2d5
ddavviidbunbun <david.bunyamin.99@gmail.com>
2026-03-11 13:24:32 +0700

    fix shell script

--- stop.sh
+++ stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # remove image and container, network and unused volumes
-# docker-compose down --rmi all --volumes
-docker-compose down 
+# docker-compose -f compose.dev.yml down --rmi all --volumes
+docker-compose -f compose.dev.yml down 
 
 # remove unused volumes
 docker system prune --volumes
